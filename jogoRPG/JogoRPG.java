ffddb4885149c8225da8c57a774c53ba817152d1
Reidiney <reidineyanjos@gmail.com>
2021-11-06 12:49:43 -0300

    Estrutura do menu
    Verificar posteriormente a estrutura de "instruções"

--- jogoRPG/JogoRPG.java
+++ jogoRPG/JogoRPG.java
@@ -1,7 +1,62 @@
 package jogoRPG;
 
+import java.util.Scanner;
+
 public class JogoRPG {
+	static Scanner input = new Scanner(System.in);
+
+	static int entradaDadosInt(String text) {
+		System.out.println(text);
+		int numberInput = input.nextInt();
+		return numberInput;
+
+	}
+
+	static void jogar() {
+
+	}
+
+	static void historia() {
+
+	}
+
+	static void instruções() {
+
+	}
+
+	static void criadores() {
+
+	}
+
+	static void menu() {
+		int valueMenu;
+		do {
+
+			valueMenu = entradaDadosInt("\n(1) Jogar \n(2) História \n(3) Instruções \n(4) Créditos \n(0) Sair");
+
+			switch (valueMenu) {
+			case 1:
+				jogar();
+				break;
+			case 2:
+				historia();
+				break;
+			case 3:
+				instruções();
+				break;
+			case 4:
+				criadores();
+				break;
+			case 0:
+				System.out.println("A guerra ainda lhe espera!");
+				break;
+			default:
+				System.out.println("\nEssa bala não encaixa no pente, escolha outra!");
+			}
+		} while (valueMenu != 0);
+	}
 
 	public static void main(String[] args) {
-
-}}
+		menu();
+	}
+}
