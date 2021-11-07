dc92aa098a1e72b9bd84b2e0565997ac6d846ccd
Victor Beccare <vpdr1912@gmail.com>
2021-11-07 17:39:59 -0300

    Refinando e finalizando primeira parte da historia/desafio
    Aqui eu faço ajustes para que a primeira parte da história funcione corretamente.
    Adicionei uma parte da história que estava faltando.
    Separei a parte da arma em um metodo para poder usá-lo em dois lugares diferentes (menu e historia parte 1)

--- jogoRPG/JogoRPG.java
+++ jogoRPG/JogoRPG.java
@@ -9,6 +9,10 @@ public class JogoRPG {
 	// variaveis globais que usaremos em diversas partes do jogo
 	static Scanner input = new Scanner(System.in);
 	static String nomeJogador = "";
+	static boolean correto = false;
+	static int vida = 3;
+	static int tentativasCadeado = 3;
+	static int tentativasEscalada = 3;
 
 	// Método para entradas de números
 	static int entradaDadosInt(String text) {
@@ -24,7 +28,7 @@ public class JogoRPG {
 
 	}
 
-	//gerar numero aleatorio
+	// gerar numero aleatorio de acordo com o limite passado
 	static int gerarNumero(int valorMaximo) {
 		int numeroAleatorio;
 		Random sorteio = new Random();
@@ -32,115 +36,268 @@ public class JogoRPG {
 		return numeroAleatorio;
 	}
 
-	
-	//formular logica de pergunta
-	static void formularPergunta(int[][] alternativas, int numeroQuestao, int valorPergunta, int valorResposta) {
-		ArrayList<Integer> listaResposta = new ArrayList<Integer>();
+	// metodo para trabalhar com as tentativas da questao do cadeado
+	static void statusCadeado() {
+		tentativasCadeado--;
+		if (tentativasCadeado == 2) {
+			System.out.println("\nTome cuidado, mais dois erros e a coisa ficará séria!");
+		} else if (tentativasCadeado == 1) {
+			System.out.println("\nSoldado, seja rápido e preciso, os inimigos estão chegando!!");
+		} else if (tentativasCadeado == 0) {
+			System.out.println("\nInfelizmente, os inimigos chegaram e te mataram desarmado!");
+			System.exit(0);
+		}
+	}
 
+	// metodo para trabalhar com as tentativas das questões de tiro
+	static void statusVida() {
+
+	}
+
+	// metodo para trabalhar com as tentativas da questão de escalada
+	static void statusEscalada() {
+
+	}
+
+	// funcao para definir quantas vidas o jogador terá nas batalhas
+	static void escolhaRifle() {
+		int escolhaArma = 0;
+		do {
+
+			imprimirArsenal();
+
+			escolhaArma = entradaDadosInt("");
+
+			switch (escolhaArma) {
+			case 1:
+			case 3:
+				vida = 1;
+				System.out.println("\nQue coragem! Lembre que você não pode falhar nenhuma vez! ");
+				break;
+			case 2:
+				vida = 2;
+				System.out.println("\nBoa escolha! Tem a chance de errar 2 vezes! ");
+				break;
+			case 4:
+				vida = 3;
+				System.out.println("\nJogando de forma tranquila, as 3 vidas estão em jogo!");
+				break;
+			default:
+				System.out.println("Poxa, soldado, aonde você está vendo essa opção? Escolha novamente!");
+			}
+		} while (escolhaArma != 1 && escolhaArma != 2 && escolhaArma != 3 && escolhaArma != 4);
+
+	}
+
+	// metodo para imprimir o armamento
+	static void imprimirArsenal() {
+		System.out.println("\nEssas são as armas em nosso arsenal:\r\n" + "\r\n"
+				+ "1-Barrett M82 - (1 vida) 12,7×99mm NATO .416 Barrett\r\n"
+				+ "2-Dragunov Sniper rifle - ( 2 vidas) 7,62×54mmR\r\n" + "3-AWM - (1 vida) 7,62×67mm \r\n"
+				+ "4-VSK-94 - ( 3 vidas) 9×39mm\r\n" + "");
+
+		System.out.println("                              WOkX                                                 \r\n"
+				+ "                 W0xxxxkOO0k,.oO00OkxddddokN                                    \r\n"
+				+ "                 No,,,,,;'.'....,;'...... 'O                                    \r\n"
+				+ "WNXNNWK0KKKKKN    WKxoccl;''..'',:c:;,'''',ldddxxxkxxxkkxkkkxxxxdlccccccclooookN\r\n"
+				+ "Kc,;::'......;cclll:'............    .........,oxxxxxkkkkkkkxxxxdlccccccccccccdN\r\n"
+				+ "K;............,od:..';c,......;,':cc:::;;'..':OW                           WWWW \r\n"
+				+ "K; ....':c:'..:xx:.:kO0k;...'lX0xO00kxkxkl,cxX                                  \r\n"
+				+ "0c:ccd0XW WXxc:;'..k    N0KKXN           WNW                                    \r\n"
+				+ "WWW           WWX00N                                                            \r\n"
+				+ "                       WWWWNXXXXNNK0KW                                          \r\n"
+				+ "                   Xdod:;;;;,''';;,c0W                                          \r\n"
+				+ "                W0d:.............',xW                                           \r\n"
+				+ "                Nx;'..........;kKKOKW       W                   WW  WWWWW       \r\n"
+				+ "N0kkxxxxkkkkkkkkkkxd:c;......,dOOOOkddkkOOOOOOOOOxoxKWWWWWWWWWWWWWWWWWWWWNXXXNW \r\n"
+				+ "0,..................................................,clllllllllllllllllll:,;;;xN\r\n"
+				+ "X;.lkkkkxc:dkkkkxl'..............:odo:cdxxxxxxxxxxxxkKKKKKKKKKKKKKKKKKKKK00000XW\r\n"
+				+ "K,.cdddddxX      K:.':l;........,ldddoOW                                        \r\n"
+				+ "NkodkO0KXW     Xd,.;k0KO:..... .d0KKKXW                                         \r\n"
+				+ "               Kl;lK   Wo',;clldX                                               \r\n"
+				+ "                 WWW   WNKXWW\r\n" + "                                                   \r\n"
+				+ " \r\n" + "            WNNNNNNW  WN0kKNWWWWW                                               \r\n"
+				+ "            Xl,;;,;codl,..';::::cO                                              \r\n"
+				+ "WWW   NNNNNXXOkOOkxkkd,.';:,.cxxxkOOkk0KKKKKKKKKKKKKKNNXXXW           WOkW      \r\n"
+				+ "0c:lxd;',,,':kKOl;,,,....,'...,,,'.......';,',,',;,..;::;;cxkkkkkkkkkkd;:dxxxdkN\r\n"
+				+ "0' .,cc::cc:coo;...............,cc::c:',;:c:;;:::::';dO00OkO0000000000kxOOkOOkON\r\n"
+				+ "K; 'O  WW     K:.';clookKo.....x  WW WXXNWWWNXNWWWNXN                           \r\n"
+				+ "0, .:dkkkkkkkxc'.lXX00XW X:....oN                                               \r\n"
+				+ "0doxkO0KKK0KK0kkx0W       Ollox0W               \r\n" + "NNNNNNW            ");
+	}
+
+	// formular logica de pergunta
+	static void formularPergunta(String[][] alternativas, int numeroQuestao, String valorPergunta, String valorResposta,
+			String pergunta, String desafio) {
+		ArrayList<String> listaResposta = new ArrayList<String>();
+
+		// convertendo array para ArrayList para utilizar o Collections
 		for (int j = 0; j < alternativas[0].length; j++) {
 			listaResposta.add(alternativas[numeroQuestao][j]);
 
 		}
 
+		// Misturando a ordem dos valores que serão mostrados para o jogador
 		Collections.shuffle(listaResposta);
 
-		System.out.println(
-				"Para abrir o cadeado, converta os seguintes dígitos de binário para decimal: \n" + valorPergunta);
+		// caso erre tem que repetir apenas essa parte, para não gerar uma pergunta
+		// diferente do que ele respondeu
 
-		for (int i = 0; i < listaResposta.size(); i++) {
-			switch (i) {
-			case 0:
-				System.out.println("\nA) " + listaResposta.get(i));
-				break;
-			case 1:
-				System.out.println("B) " + listaResposta.get(i));
-				break;
-			case 2:
-				System.out.println("C) " + listaResposta.get(i));
-				break;
-			case 3:
-				System.out.println("D) " + listaResposta.get(i));
-				break;
-			case 4:
-				System.out.println("E) " + listaResposta.get(i));
-				break;
+		do {
+			System.out.println(pergunta + valorPergunta);
+
+			// código para mostrar as alternativas sorteadas
+			for (int i = 0; i < listaResposta.size(); i++) {
+				switch (i) {
+				case 0:
+					System.out.println("\nA) " + listaResposta.get(i));
+					break;
+				case 1:
+					System.out.println("B) " + listaResposta.get(i));
+					break;
+				case 2:
+					System.out.println("C) " + listaResposta.get(i));
+					break;
+				case 3:
+					System.out.println("D) " + listaResposta.get(i));
+					break;
+				case 4:
+					System.out.println("E) " + listaResposta.get(i));
+					break;
+				}
+
 			}
 
-		}
+			String respostaJogador = entradaDadosStr("").toUpperCase();
+			int index = 5;
+			// código para converter a altenativa passada para numero, para usarmos no array
+			switch (respostaJogador) {
 
-		String respostaJogador = entradaDadosStr("").toUpperCase();
-		int index = 0;
+			case "A":
+				index = 0;
+				break;
+			case "B":
+				index = 1;
+				break;
+			case "C":
+				index = 2;
+				break;
+			case "D":
+				index = 3;
+				break;
+			case "E":
+				index = 4;
+				break;
+			default:
+				System.out.println("Opção inválida!");
 
-		switch (respostaJogador) {
+			}
 
-		case "A":
-			index = 0;
-			break;
-		case "B":
-			index = 1;
-			break;
-		case "C":
-			index = 2;
-			break;
-		case "D":
-			index = 3;
-			break;
-		case "E":
-			index = 4;
-			break;
-		}
+			// Código para saber se ele digitou algo válido
+			if (index != 5) {
+				// código para saber se a alternativa passada é a correta
+				if (listaResposta.get(index).equals(valorResposta)) {
+					correto = true;
 
-		if (listaResposta.get(index) == valorResposta) {
-			System.out.println("Resposta Correta");
+				} else {
+					// código que quando ele erra a questão, saber qual questão foi respondida para
+					// ter uma resposta coerente
+					if (desafio.equals("cadeado")) {
+						statusCadeado();
+					} else if (desafio.equals("escalada")) {
+						statusEscalada();
+					} else {
+						statusVida();
+					}
 
-		} else {
-			System.out.println("Resposta incorreta");
-		}
+				}
+			}
+
+		} while (vida != 0 && tentativasCadeado != 0 && tentativasEscalada != 0 && !correto);
 
 	}
 
 	// espaço para trabalhar com as perguntas
+
 	static void desafio1(int numeroQuestao) {
+		// valores que podem ser utilizados na pergunta
+		String[] valoresPergunta = { "10110110", "10111100", "01111111", "10010110", "11011100" };
 
-		int[] valoresPergunta = { 10110110, 10111100, 01111111, 10010110, 11011100 };
-		int[] valoresResposta = { 182, 188, 127, 150, 220 };
-		int valorPergunta = valoresPergunta[numeroQuestao];
-		int valorResposta = valoresResposta[numeroQuestao];
-		int[][] alternativas = new int[][] { { 182, 181, 176, 170, 175 }, { 188, 185, 190, 181, 182 },
-				{ 127, 128, 129, 130, 131 }, { 150, 145, 148, 152, 153 }, { 221, 230, 220, 231, 227 } };
+		// valores que podem ser utilizados na resposta
+		String[] valoresResposta = { "182", "188", "127", "150", "220" };
 
-		formularPergunta(alternativas, numeroQuestao, valorPergunta, valorResposta);
+		// passando o mesmo valor para pergunta e resposta para ter o valor correto
+		String valorPergunta = valoresPergunta[numeroQuestao];
+		String valorResposta = valoresResposta[numeroQuestao];
+
+		// valores que podem aparecer como opcao de alternativas
+		String[][] alternativas = new String[][] { { "182", "181", "176", "170", "175" },
+				{ "188", "185", "190", "181", "182" }, { "127", "128", "129", "130", "131" },
+				{ "150", "145", "148", "152", "153" }, { "221", "230", "220", "231", "227" } };
+
+		String pergunta = "\nPara abrir o cadeado, converta os seguintes dígitos de binário para decimal: \n";
+
+		formularPergunta(alternativas, numeroQuestao, valorPergunta, valorResposta, pergunta, "cadeado");
 
 	}
 
-	// o jogar será divididos em partes, abaixo está cada parte que será encaixada
-	// ao jogo
+	static void desafio2(int numeroQuestao) {
+
+	}
+
+	static void desafio3(int numeroQuestao) {
+
+	}
+
+	static void desafio4(int numeroQuestao) {
+
+	}
+
+	static void desafio5(int numeroQuestao) {
+
+	}
+
+	static void desafio6(int numeroQuestao) {
+
+	}
+
+	// ----- funcoes usadas no jogar -----
 
 	static void prologo() {
+		int resp;
 		System.out.println(
-				"Tudo começou pelo amanhecer daquele dia que seria marcado para sempre nos livros de história: "
+				"\nTudo começou pelo amanhecer daquele dia que seria marcado para sempre nos livros de história: "
 						+ "\nO DIA QUE O BRASIL FOI INVADIDO!");
 		System.out.println(
 				"A Coalisão era um grupo de países que se reuniram em busca da cura da praga que afetou a produção mundial de café."
-						+ "\n Essa praga foi transmitida por fungos que eram propagados pelo ar por meio de esporos,"
-						+ "estes que em contato com os grãos, alteravam a genética e impossibilitavam o desenvolvimento do grão de café.");
+						+ "\nEssa praga foi transmitida por fungos que eram propagados pelo ar por meio de esporos,"
+						+ "\nestes que em contato com os grãos, alteravam a genética e impossibilitavam o desenvolvimento do grão de café.");
 
 		System.out.println(
 				"Naquela manhã, o Major se encontrava no quartel extremamente enfurecido! E pronto para dar as ordens para o seu fiel subordinado, o Capitão:");
 
-		System.out.println("\n-Major:- Capitão! venha até aqui. "
+		System.out.println("\n-Major: Capitão! venha até aqui."
 				+ "\n Precisamos alocar o quanto antes o novo soldado da classe Sniper no batalhão.");
 		System.out.println("-Capitão: Certo! Irei fazer isso! Inclusive, qual o seu nome, filho?");
 
 		System.out.println("-Personagem: " + nomeJogador);
 
 		System.out.println("-Major: " + nomeJogador + "? Hahahaha!! Isso aqui é uma guerra! "
-				+ "\nAqueles selvagens estão nos atacando e vocês vão ficar trocando figurinhas sobre um ao outro? "
+				+ "\n-Capitão: Aqueles selvagens estão nos atacando e vocês vão ficar trocando figurinhas sobre um ao outro? "
 				+ "\nAqui você vai ser o soldado 06!” Gritou.");
+		do {
+			System.out.println(
+					"\nE depois de ser apresentado ao quartel, 06 começou o seu treinamento junto com o capitão. "
+							+ "\nUma pena que eles não puderam continuar a conversa, pois ali próximo as tropas da coalisão"
+							+ "\nestavam invadindo o terreno. Eram milhares de soldados descendo pelo céu de paraquedas.");
 
-		System.out.println("E depois de ser apresentado ao quartel, 06 começou o seu treinamento junto com o capitão. "
-				+ "\n Uma pena que eles não puderam continuar a conversa, pois ali próximo as tropas da coalisão"
-				+ "\n estavam invadindo o terreno. Eram milhares de soldados descendo pelo céu de paraquedas.");
+			resp = entradaDadosInt("\n(1) continuar");
+			if (resp != 1) {
+				System.out.println("\nSoldado, eu não compreendi...");
+				System.out.print("\n...");
+			}
+
+		} while (resp != 1);
 
 	}
 
@@ -176,6 +333,31 @@ public class JogoRPG {
 				+ "\r\n" + "-06: Entendido Capitão!\r\n");
 
 		desafio1(numeroSorteado);
+		correto = false;
+
+		System.out.println("\n-Capitão: Boa 06!!! Agora escolhe logo um rifle pois não temos muito tempo!");
+
+		escolhaRifle();
+
+	}
+
+	static void historiaParte2() {
+
+	}
+
+	static void historiaParte3() {
+
+	}
+
+	static void historiaParte4() {
+
+	}
+
+	static void historiaParte5() {
+
+	}
+
+	static void historiaParte6() {
 
 	}
 
@@ -185,6 +367,11 @@ public class JogoRPG {
 	static void jogar() {
 		prologo();
 		historiaParte1();
+		historiaParte2();
+		historiaParte3();
+		historiaParte4();
+		historiaParte5();
+		historiaParte6();
 	}
 
 	// metodo para mostrar como será a história do jogo
@@ -237,40 +424,8 @@ public class JogoRPG {
 					+ "Porém, um erro na mira e você já era!");
 			System.out.println(
 					"\nUma arma mais fraca lhe permite mais estabilidade e torna mais difícil para o inimigo te detectar, isso lhe permite mais tentativas... ");
-			System.out.println("Essas são as armas em nosso arsenal:\r\n" + "\r\n"
-					+ "Barrett M82 - (1 vida) 12,7×99mm NATO .416 Barrett\r\n"
 
-					+ "Dragunov Sniper rifle - ( 2 vidas) 7,62×54mmR\r\n" + "AWM - (1 vida) 7,62×67mm \r\n"
-					+ "VSK-94 - ( 3 vidas) 9×39mm\r\n" + "");
-
-			System.out.println("                              WOkX                                                 \r\n"
-					+ "                 W0xxxxkOO0k,.oO00OkxddddokN                                    \r\n"
-					+ "                 No,,,,,;'.'....,;'...... 'O                                    \r\n"
-					+ "WNXNNWK0KKKKKN    WKxoccl;''..'',:c:;,'''',ldddxxxkxxxkkxkkkxxxxdlccccccclooookN\r\n"
-					+ "Kc,;::'......;cclll:'............    .........,oxxxxxkkkkkkkxxxxdlccccccccccccdN\r\n"
-					+ "K;............,od:..';c,......;,':cc:::;;'..':OW                           WWWW \r\n"
-					+ "K; ....':c:'..:xx:.:kO0k;...'lX0xO00kxkxkl,cxX                                  \r\n"
-					+ "0c:ccd0XW WXxc:;'..k    N0KKXN           WNW                                    \r\n"
-					+ "WWW           WWX00N                                                            \r\n"
-					+ "                       WWWWNXXXXNNK0KW                                          \r\n"
-					+ "                   Xdod:;;;;,''';;,c0W                                          \r\n"
-					+ "                W0d:.............',xW                                           \r\n"
-					+ "                Nx;'..........;kKKOKW       W                   WW  WWWWW       \r\n"
-					+ "N0kkxxxxkkkkkkkkkkxd:c;......,dOOOOkddkkOOOOOOOOOxoxKWWWWWWWWWWWWWWWWWWWWNXXXNW \r\n"
-					+ "0,..................................................,clllllllllllllllllll:,;;;xN\r\n"
-					+ "X;.lkkkkxc:dkkkkxl'..............:odo:cdxxxxxxxxxxxxkKKKKKKKKKKKKKKKKKKKK00000XW\r\n"
-					+ "K,.cdddddxX      K:.':l;........,ldddoOW                                        \r\n"
-					+ "NkodkO0KXW     Xd,.;k0KO:..... .d0KKKXW                                         \r\n"
-					+ "               Kl;lK   Wo',;clldX                                               \r\n"
-					+ "                 WWW   WNKXWW\r\n" + "                                                   \r\n"
-					+ " \r\n" + "            WNNNNNNW  WN0kKNWWWWW                                               \r\n"
-					+ "            Xl,;;,;codl,..';::::cO                                              \r\n"
-					+ "WWW   NNNNNXXOkOOkxkkd,.';:,.cxxxkOOkk0KKKKKKKKKKKKKKNNXXXW           WOkW      \r\n"
-					+ "0c:lxd;',,,':kKOl;,,,....,'...,,,'.......';,',,',;,..;::;;cxkkkkkkkkkkd;:dxxxdkN\r\n"
-					+ "0' .,cc::cc:coo;...............,cc::c:',;:c:;;:::::';dO00OkO0000000000kxOOkOOkON\r\n"
-					+ "K; 'O  WW     K:.';clookKo.....x  WW WXXNWWWNXNWWWNXN                           \r\n"
-					+ "0, .:dkkkkkkkxc'.lXX00XW X:....oN                                               \r\n"
-					+ "0doxkO0KKK0KK0kkx0W       Ollox0W               \r\n" + "NNNNNNW            ");
+			imprimirArsenal();
 
 			value = entradaDadosInt("\n(1) Voltar");
 
